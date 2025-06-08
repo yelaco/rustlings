7d893698ba2ad170521e7e5ebe825188ce45ca22
Quang M. Bui <minhquangbui053@gmail.com>
2025-06-08 17:06:48 +0000

    Complete exercises for lifetimes, tests, and iterators
    - Fixed lifetimes issues in lifetimes1.rs, lifetimes2.rs, and lifetimes3.rs.
    
    - Completed test cases in tests1.rs, tests2.rs, and tests3.rs.
    
    - Implemented iterator solutions in iterators1.rs to iterators5.rs.
    
    Signed-off-by: Quang M. Bui <minhquangbui053@gmail.com>

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,11 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => {
+            let mut result = first.to_uppercase().collect::<String>();
+            result.extend(chars);
+            result
+        }
     }
 }
 
@@ -15,14 +19,14 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 fn main() {
