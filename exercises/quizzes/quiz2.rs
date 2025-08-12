1814b16508f727ccdbbdbe50afdb4e46f6c3f475
Quang M. Bui <minhquangbui053@gmail.com>
2025-08-12 21:35:07 +0700

    Complete modules, hashmaps, options, errors, and quiz2 exercises

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(s, c)| match c {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(n) => {
+                    let mut result = s.clone();
+                    for _ in 0..n {
+                        result.push_str("bar");
+                    }
+                    result
+                }
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,8 +52,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
