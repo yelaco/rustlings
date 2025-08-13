8461944141f435a58526f3726b0c1bebf753f469
Quang M. Bui <minhquangbui053@gmail.com>
2025-08-13 17:25:16 +0700

    Solve tests and iterators exercises

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,9 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    map.values()
+        .filter(|&&val| val == value) // Filter values that match the given value
+        .count() // Count the filtered values
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +51,11 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    collection
+        .iter() // Iterate over each hash map in the collection
+        .flat_map(|map| map.values()) // Flatten the values of all hash maps into a single iterator
+        .filter(|&&val| val == value) // Filter values that match the given value
+        .count() // Count the filtered values
 }
 
 fn main() {
